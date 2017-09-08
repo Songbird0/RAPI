db347383eafc30c060cdaecf5f41b4e1e50f3c35
Anthony Defranceschi <chaacygg@gmail.com>
2017-09-08 05:10:40 +0200

    unimplemented unit tests

---
+++ src/main/java/fr/songbird/rapi/App.java
@@ -0,0 +1,13 @@
+package fr.songbird.rapi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/fr/songbird/rapi/OptionTest.java
@@ -0,0 +1,128 @@
+/*
+    A useful results API to release all of the functional programming power!
+    Copyright (C) 2017  Anthony Defranceschi
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package fr.songbird.rapi;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+/**
+ * @author anthony
+ * @version majeure.mineure.bug
+ * @since 08/09/17
+ */
+public class OptionTest {
+
+    @Rule
+    public final ExpectedException expectedException = ExpectedException.none();
+
+    @Test
+    public void isSomeTest() {
+
+    }
+
+    @Test
+    public void isNoneTest() {
+
+    }
+
+    @Test
+    public void expectTest() {
+
+    }
+
+    @Test
+    public void unwrapTest() {
+
+    }
+
+    @Test
+    public void unwrapOrTest() {
+
+    }
+
+    @Test
+    public void mapTest() {
+
+    }
+
+    @Test
+    public void mapOrTest() {
+
+    }
+
+    @Test
+    public void mapOrElse() {
+
+    }
+
+    @Test
+    public void okOrTest() {
+
+    }
+
+    @Test
+    public void okOrElseTest() {
+
+    }
+
+    @Test
+    public void iterTest() {
+
+    }
+
+    @Test
+    public void andTest() {
+
+    }
+
+    @Test
+    public void andThenTest() {
+
+    }
+
+    @Test
+    public void orTest() {
+
+    }
+
+    @Test
+    public void orElseTest() {
+
+    }
+
+    @Test
+    public void getOrInsertTest() {
+
+    }
+
+    @Test
+    public void getOrInsertWithTest() {
+
+    }
+
+    @Test
+    public void takeTest() {
+
+    }
+
+    @Test
+    public void cloneTest() {
+
+    }
+}
